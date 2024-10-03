00ea1cb3ef1d6c9715ef263e4963531cbc480a50
Alarak-cmd <yasanser@yandex.ru>
2024-10-03 19:54:49 +0300

    Update hw_3.py

--- hw_3.py
+++ hw_3.py
@@ -4,10 +4,18 @@ for i in range(1, n):
     summ += i ** 3
 print(summ) """
 
-for i in range(9, 0, -1):
+""" for i in range(1, 10):
     for j in range(1, 10):
         if i * j < 10:
             print(i * j, end='  ')
         else:
             print(i * j, end=' ')
-    print()
+    print() """
+
+""" for i in range(9, 0, -1):
+    for j in range(1, 10):
+        if i * j < 10:
+            print(i * j, end='  ')
+        else:
+            print(i * j, end=' ')
+    print() """
