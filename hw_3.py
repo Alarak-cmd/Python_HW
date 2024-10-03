251a5b9486dc92e994969724f7834b9332d9dc3e
Alarak-cmd <yasanser@yandex.ru>
2024-10-03 19:52:24 +0300

    Homework 3

---
+++ hw_3.py
@@ -0,0 +1,13 @@
+""" n = int(input())
+summ = 0
+for i in range(1, n):
+    summ += i ** 3
+print(summ) """
+
+for i in range(9, 0, -1):
+    for j in range(1, 10):
+        if i * j < 10:
+            print(i * j, end='  ')
+        else:
+            print(i * j, end=' ')
+    print()
