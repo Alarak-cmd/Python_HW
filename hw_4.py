13230ff74fa8d16bc80f332981d7ce892f127e94
Alarak-cmd <yasanser@yandex.ru>
2024-10-11 12:42:33 +0300

    Homework

---
+++ hw_4.py
@@ -0,0 +1,21 @@
+st = input()
+leng = 0
+p = 1
+st = st.replace('h', '!')
+for i in range(len(st) - 1):
+    if st[i] == st[i + 1]:
+        p += 1
+        leng = max(leng, p)
+    else:
+        p = 1
+print(leng, st)
+
+st = input()
+sh_st = st[st.find('(') + 1 : st.find(')')]
+print(sh_st)
+
+st = input()
+l = st.split(' ')
+for i in range(len(st)):
+    if l[i][0] == 'а' and l[i][-1] == 'я':
+        print(l[i])
